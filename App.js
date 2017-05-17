8f330ee9489d642bde4e3bca58ca6b0eb2c8b2ba
Avinash Chauhan <avinash664591@gmail.com>
2017-05-17 22:53:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+//declared variable radius r using var keyword and assigned value 2 to it
+var r=2;
+//declared variable Area a and applied formula of area of circle 
+var a=Math.PI*r*r;
+//printing result in console as output 
+console.log("Area of Circle is "+a);
